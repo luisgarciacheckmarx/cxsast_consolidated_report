f8f3e42103e7f4ac5992a08dc2f2260c5088963e
Hugo Mota <hugo.mota@gmail.com>
2020-01-22 12:11:41 +0000

    feat: add the report saving fallback ability

--- source/utils/index.ts
+++ source/utils/index.ts
@@ -1,4 +1,5 @@
-export { log as logger, logElapsedTime } from './logger'
-export { default as handleError } from './errorHandler'
-export { default as config } from './config'
-export { default as validateArgs } from './argumentsValidator'
+export { log as logger, logElapsedTime } from './logger';
+export { default as handleError } from './errorHandler';
+export { default as config } from './config';
+export { default as validateArgs } from './argumentsValidator';
+export { default as reportGenerator } from './reportGenerator';
