7640ae33db18d85e99adddbd044add40d7039f9a
luisgarciacheckmarx <98318336+luisgarciacheckmarx@users.noreply.github.com>
2023-01-30 14:58:58 +0100

    Update index.ts

--- source/index.ts
+++ source/index.ts
@@ -1,4 +1,11 @@
+import dateFormat from 'dateformat';
+import yargs from 'yargs';
+import { getReportData } from './controllers/scansController';
+import { EmailService } from './services';
+import { logger, handleError, validateArgs, reportGenerator } from './utils';
 
+const log = logger('main');
+const args = yargs.argv;
 
 const main = async () => {
     
