6e4835cd14fea701398db5d822517471aa6ba760
luisgarciacheckmarx <98318336+luisgarciacheckmarx@users.noreply.github.com>
2023-01-30 21:36:23 +0100

    fix(kk): kkkk

--- source/index.ts
+++ source/index.ts
@@ -8,6 +8,20 @@
 //const log = logger('main');
 //const args = yargs.argv;
 
+import log4js from "log4js";
+log4js.configure({
+  appenders: { cheese: { type: "file", filename: "cheese.log" } },
+  categories: { default: { appenders: ["cheese"], level: "error" } },
+});
+
+const logger = log4js.getLogger();
+logger.level = "debug";
+logger.debug("Some debug messages");
+
+
+
+
+
 console.log('Hola caracola 1 ');
 
 const main = () => {
