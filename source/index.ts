612c5bd8bdaede1343a4d26b889046afefb37f66
luisgarciacheckmarx <98318336+luisgarciacheckmarx@users.noreply.github.com>
2023-01-30 15:51:35 +0100

    fix(pencil): stop graphite breaking when too much pressure applied

--- source/index.ts
+++ source/index.ts
@@ -12,9 +12,7 @@ const args = yargs.argv;
 const main = async () => {
     
     console.log('Hola caracola ');
-    #if (args.emailRecipients) {
-    #    args.emailRecipients = args.emailRecipients.split(',').map((email: string) => email.trim());
-    #}
+
 
     const schema = object().shape({
         nameRegex: string().required(),
