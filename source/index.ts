dbf478b1c57297933833e0da867bd2abe7eb7bc9
luisgarciacheckmarx <98318336+luisgarciacheckmarx@users.noreply.github.com>
2023-01-30 14:46:39 +0100

    Update index.ts

--- source/index.ts
+++ source/index.ts
@@ -1,34 +1,8 @@
-import dateFormat from 'dateformat';
-import yargs from 'yargs';
-import { getReportData } from './controllers/scansController';
-import { EmailService } from './services';
-import { logger, handleError, validateArgs, reportGenerator } from './utils';
 
-const log = logger('main');
-const args = yargs.argv;
 
 const main = async () => {
-    validateArgs(args);
-
-    log.info('fetching scans data ...');
-
-    try {
-        const date = new Date();
-        const data = await getReportData(String(args.nameRegex));
-        log.info('Finished the data fetch!');
-
-        const compiledTemplate = reportGenerator.getCompiledHtml({
-            ...data,
-            totalUnresolvedIssues: data.combinedResults.newIssues + data.combinedResults.recurrentIssues,
-            currentDate: dateFormat(date, 'dddd, mmmm dS, yyyy, h:MM:ss TT'),
-            year: dateFormat(date, 'yyyy'),
-            appName: String(args.appName),
-        });
-
-        EmailService.sendEmail(compiledTemplate, String(args.emailSubject), String(args.emailRecipients), String(args.appName));
-    } catch (error) {
-        handleError(error);
-    }
+    
+    console.log('Hola caracola');
 };
 
 main();
