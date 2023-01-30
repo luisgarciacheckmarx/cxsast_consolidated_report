9b7a4e9ffca1eefed2654817aeea02b2c30cb126
luisgarciacheckmarx <98318336+luisgarciacheckmarx@users.noreply.github.com>
2023-01-31 00:08:38 +0100

    fix(hora): esto va a funcionar

--- source/index.ts
+++ source/index.ts
@@ -1,57 +1,35 @@
 
-//import dateFormat from 'dateformat'; 
-//import yargs from 'yargs';
-//import { getReportData } from './controllers/scansController';
-//import { EmailService } from './services';
-//import { handleError } from './utils';
+import dateFormat from 'dateformat';
+import yargs from 'yargs';
+import { getReportData } from './controllers/scansController';
+import { EmailService } from './services';
+import { logger, handleError, validateArgs, reportGenerator } from './utils';
 
-//const log = logger('main');
-//const args = yargs.argv;
+const log = logger('main');
+const args = yargs.argv;
 
-import * as log4js from "log4js";
-log4js.configure({
-  appenders: { cheese: { type: "file", filename: "cheese.log" } },
-  categories: { default: { appenders: ["cheese"], level: "error" } },
-});
+const main = async () => {
+    validateArgs(args);
 
-const logger = log4js.getLogger();
-logger.level = "debug";
-logger.debug("Some debug messages");
-
-
-
-
-
-console.log('Hola caracola 1 ');
-
-const main = () => {
-    //validateArgs(args);
-    console.log('Hola caracola 2');
-    
-    //log.info('fetching scans data ...');
+    log.info('fetching scans data ...');
 
     try {
-        //const date = new Date();
-        //const data = await getReportData(String('nameRegex'));
-        console.log('Finished the data fetch!');
+        const date = new Date();
+        const data = await getReportData(String(args.nameRegex));
+        log.info('Finished the data fetch!');
 
-        //const compiledTemplate = reportGenerator.getCompiledHtml({
-        //    ...data,
-        //    totalUnresolvedIssues: data.combinedResults.newIssues + data.combinedResults.recurrentIssues,
-        //    currentDate: dateFormat(date, 'dddd, mmmm dS, yyyy, h:MM:ss TT'),
-        //    year: dateFormat(date, 'yyyy'),
-        //    appName: String('appName'),
-        //});
-        //const compiledTemplate = "";
-        console.log('Hola caracola 2.1');
+        const compiledTemplate = reportGenerator.getCompiledHtml({
+            ...data,
+            totalUnresolvedIssues: data.combinedResults.newIssues + data.combinedResults.recurrentIssues,
+            currentDate: dateFormat(date, 'dddd, mmmm dS, yyyy, h:MM:ss TT'),
+            year: dateFormat(date, 'yyyy'),
+            appName: String(args.appName),
+        });
 
-        //EmailService.sendEmail(compiledTemplate, 'emailSubject', 'emailRecipients', 'appName');
+        EmailService.sendEmail(compiledTemplate, String(args.emailSubject), String(args.emailRecipients), String(args.appName));
     } catch (error) {
-        console.log('Hola caracola 3');
-        //handleError(error);
+        handleError(error);
     }
 };
 
-console.log('Hola caracola 0 ');
 main();
-console.log('Hola caracola 4');
